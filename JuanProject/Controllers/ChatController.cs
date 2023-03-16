9fa918579910569c64322fec5f6047b726bf3fba
Asgar Y. Asgarov <asgaryas@code.local>
2023-03-16 18:22:47 +0400

    bug-fix:View name problem handled

---
+++ JuanProject/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using JuanProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuanProject.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ChatController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Chat()
+        {
+            ViewBag.Users = _userManager.Users.ToList();
+            return View();
+        }
+    }
+}
